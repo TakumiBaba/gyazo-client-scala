01256af368248af315b8a3c282029da410857dea
BabaTakumi <bb@BabaTakumi-no-MacBook-Air-2.local>
2014-10-20 01:16:30 +0900

    add files

---
+++ src/main/scala/Gyazo.scala
@@ -0,0 +1,24 @@
+import scala.concurrent._
+
+/**
+ * Created by bb on 14/10/20.
+ */
+class Gyazo(accessToken:String) {
+  val api:String = "https://api.gyazo.com/api/"
+
+  def upload():Future = {
+    val fs = Future {
+
+
+    }
+    return fs
+  }
+
+  def list() = {
+
+  }
+
+  def delete() = {
+
+  }
+}
